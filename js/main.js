40d5eaf58949a5a9f034c76c1dc2f575a7d5ab16
Scott Huffman <scott@shuffman.com>
2017-10-31 00:26:00 -0500

    Buttons switch views. Views split into files.

--- js/main.js
+++ js/main.js
@@ -1,33 +1,49 @@
-let $ = require('jquery');
+let $ = require('jquery')
 let electron = require('electron')
 let remote = electron.remote
 let dialog = remote.dialog
 
 $(document).ready(function(){
-    this.tilesetEditor = new TilesetEditor();
+	this.settingsView = new Settings()
+	this.tilesetEditorView = new TilesetEditor()
 })
 
+class Settings{
+	constructor(){
+		//add button listeners
+		$('#settings-menu-btn').click(this.selectWindow)
+	}
+
+	selectWindow(){
+		console.log('settingsSelectedWindow')
+
+		$("#main-menu-btns > button.active").removeClass("active")
+		$('#settings-menu-btn').addClass('active')
+		$('#current-menu-btns').html(``)
+		$('#app-window').load('html/settings.html')
+	}
+
+}
+
 class TilesetEditor{
-    constructor(){
-        this.selectTilesetWindow();
-    }
+	constructor(){
+		//add button listeners
+		$('#open-tileset').click(this.button_openTileset_onClick)
+		$('#tileset-menu-btn').click(this.selectWindow)
+		this.selectWindow();
+	}
 
-    selectTilesetWindow(){
-        var me = this;
-        $('#tileset-menu-btn').addClass('active');
-        $('#current-menu-btns').html(
-            `<button id='open-tileset' class='btn btn-default'>
-                <span class='icon icon-home icon-folder'></span>
-                &nbsp;&nbsp;Open Tileset
-            </button>`
-        );
-        $('#open-tileset').click(me.button_openTileset_onClick);
+	selectWindow(){
+		console.log('tilesetSelectedWindow')
+		$("#main-menu-btns > button.active").removeClass("active")
+		$('#tileset-menu-btn').addClass('active')
+		$('#current-menu-btns').load('html/tileset_menu.html')
+		$('#app-window').load('html/tileset.html')
 
+	}
 
-    }
-
-    button_openTileset_onClick(){
-        console.log('click');
-        console.log(dialog.showOpenDialog({properties: ['openFile', 'multiSelections']}))
-    }
+	button_openTileset_onClick(){
+		console.log('click')
+		console.log(dialog.showOpenDialog({properties: ['openFile', 'multiSelections']}))
+	}
 }
