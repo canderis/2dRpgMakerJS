19a086aaf7a2a936471771f64703d7e566b6db77
Scott Huffman <scott@shuffman.com>
2017-10-30 23:37:42 -0500

    Ability to open files.

--- js/main.js
+++ js/main.js
@@ -1,12 +1,33 @@
 let $ = require('jquery');
+let electron = require('electron')
+let remote = electron.remote
+let dialog = remote.dialog
 
 $(document).ready(function(){
-    $('#tileset-menu-btn').addClass('active');
     this.tilesetEditor = new TilesetEditor();
 })
 
 class TilesetEditor{
     constructor(){
-        
+        this.selectTilesetWindow();
+    }
+
+    selectTilesetWindow(){
+        var me = this;
+        $('#tileset-menu-btn').addClass('active');
+        $('#current-menu-btns').html(
+            `<button id='open-tileset' class='btn btn-default'>
+                <span class='icon icon-home icon-folder'></span>
+                &nbsp;&nbsp;Open Tileset
+            </button>`
+        );
+        $('#open-tileset').click(me.button_openTileset_onClick);
+
+
+    }
+
+    button_openTileset_onClick(){
+        console.log('click');
+        console.log(dialog.showOpenDialog({properties: ['openFile', 'multiSelections']}))
     }
 }
